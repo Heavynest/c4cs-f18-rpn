060c50d9bd845c5e08da7edbdd9a30f4ce01ff15
heavynest Sr <chaojy@umich.edu>
2018-11-07 14:25:33 -0500

    right version

--- rpn.py
+++ rpn.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
+
 import operator
 
-op={'+':operator.add, '-':operator.sub,'*':operator.mul, '/':operator.floordiv,'^':operator.pow}
-def calculate(arg):
-	#stack for calculater
-	stack=[]
-	# tokenize input
-	tokens = arg.split()
-	# process tokens
-	for token in tokens:
-		try:
-			value=int(token)
-			stack.append(value)
-		except ValueError:
-			val1=int(stack.pop())
-			val2=int(stack.pop())
-			func=op[token]
-			result=func(val2,val1)
-			stack.append(result)			
-			return stack[0]
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-	while True:
-		print(calculate(input("rpn calc> ")))
-			
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
- main()
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
+    main()
 
--- test_rpn.py
+++ test_rpn.py
@@ -1,19 +1,18 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
-	def test_add(self):
-		result = rpn.calculate('1 1 +')
-		self.assertEqual(2,result)
-	def test_sub(self):
-		result = rpn.calculate('4 3 -')
-		self.assertEqual(1,result)
-	def test_multiple(self):
-		result=rpn.calculate('2 5 *')
-		self.assertEqual(10,result)
-	def test_division(self):
-		result=rpn.calculate('12 4 /')
-		self.assertEqual(3,result)
-	def test_carat(self):
-		result= rpn.calculate('2 5 ^')
-		self.assertEqual(32,result)
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2, result)
+    def test_subtract(self):
+        result = rpn.calculate("5 3 -")
+        self.assertEqual(2, result)
+    def test_multiply(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_divide(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
+
